b72bc3677ad2fb65baa96e93526253a9d6f1b399
z004mr2 <mike.juarezsweeney@target.com>
2020-03-11 16:00:23 -0500

    Implemented retry in the event of connection failure

--- src/main/java/com/names/service/NameService.java
+++ src/main/java/com/names/service/NameService.java
@@ -1,18 +1,34 @@
 package com.names.service;
 
+import com.names.db.DataStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.ConnectException;
 
 @Service
 public class NameService {
 
-    private String url = "http://localhost:8082/";
+    @Autowired
+    DataStore dataStore;
+
     private RestTemplate template = new RestTemplate();
 
-    public Integer getNewId() {
+    @Retryable(maxAttempts = 5, value = ConnectException.class,
+            backoff = @Backoff(delay = 1000, multiplier = 2))
+    public Integer getNewId() throws ConnectException {
+        System.out.println("Trying...");
 
-        return template.exchange(url, HttpMethod.GET, null, Integer.class).getBody();
+        return template.exchange("http://localhost:8082/", HttpMethod.GET, null, Integer.class).getBody();
     }
 
+    @Recover
+    private void recoveryMethodTBD() {
+        //TODO Figure out what to do
+    }
 }
