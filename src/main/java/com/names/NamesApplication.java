751461005d1f020a8d8ca514f6a83b49e4ca7dc0
z004mr2 <mike.juarezsweeney@target.com>
2020-03-11 16:00:45 -0500

    Retry Enabled annotation added

--- src/main/java/com/names/NamesApplication.java
+++ src/main/java/com/names/NamesApplication.java
@@ -2,8 +2,10 @@ package com.names;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class NamesApplication {
 
     public static void main(String[] args) {
